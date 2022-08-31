67ff5d5c4145ef41b2fb11745b4a0f1254951e15
Roman <r8mano@gmail.com>
2022-08-31 14:07:07 +0100

    BOOK-add smth1

---
+++ src/main/java/com/rr/bookslibrary/Book.java
@@ -0,0 +1,33 @@
+package com.rr.bookslibrary;
+
+public class Book
+{
+    private String author;
+    private String name;
+
+    public Book(String author, String name)
+    {
+        this.author = author;
+        this.name = name;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
